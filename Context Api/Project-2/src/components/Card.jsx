03a50ea962bc9eeb98d7288653f641fe34d27888
Danish-Siddiqui-0318 <contact.dasiddique@gmail.com>
2024-07-23 11:21:46 +0500

    Another Project Of Context API

--- Context Api/Project-2/src/components/Card.jsx
+++ Context Api/Project-2/src/components/Card.jsx
@@ -6,7 +6,7 @@ export default function Card() {
       <a href="/">
         <img
           className="p-8 rounded-t-lg"
-          src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          src="https://www.apple.com/newsroom/images/product/watch/standard/Apple_watch-series7_hero_09142021_big.jpg.large.jpg"
           alt="product_image1"
         />
       </a>
